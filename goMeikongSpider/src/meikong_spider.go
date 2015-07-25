64524adae9ec296e9ed1cd543dace186fe1bb5ea
Rosen Lu <lucumt@gmail.com>
2015-07-25 17:01:48 +0800

    提交结合mog向mongodb中插入数据的代码。

--- goMeikongSpider/src/meikong_spider.go
+++ goMeikongSpider/src/meikong_spider.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
+	"labix.org/v2/mgo"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 )
+
+type Model struct {
+	Name  string
+	Click int
+	Page  string
+}
 
 func main() {
 	meikongSpider("http://www.moko.cc/channels/post/23/1.html")
@@ -24,26 +32,44 @@ func meikongSpider(url string) {
 		log.Fatal(err)
 	}
 
+	session, err := mgo.Dial("127.0.0.1")
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+	m := session.DB("meikong").C("model")
+	
+	//先删除所有的记录
+	m.RemoveAll(nil)
+
 	doc.Find("div.index-module.noborder-module>div.w970>ul.post.small-post").Each(func(i int, s *goquery.Selection) {
 		name := s.Find("li>a.nickname").Text()
 		name = strings.Replace(name, ".", "", -1)
 		click := s.Find("li").Eq(2).Find("span").Text()
 		href, _ := s.Find("div.cover>a").Attr("href")
-		num = num+1
+		num = num + 1
 		fmt.Println("模特", num, ":", name)
 		fmt.Println("点击量:", click)
 		url = "http://www.moko.cc" + href
 		fmt.Println("个人主页:", url)
 		getModelInfo(url, name)
 		fmt.Println("-----------------------")
+
+		clicknum, _ := strconv.Atoi(click)
+		err = m.Insert(&Model{Name: name, Click: clicknum, Page: url})
+
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("**********insert data success****************")
+		}
 	})
-	
+
 	node := doc.Find("p.page>a").Last()
-	_,exists := node.Attr("hidefocus")
-	if exists{
-	    next,_ := node.Attr("href")
-	    next = "http://www.moko.cc"+next
-        meikongSpider(next)
+	_, exists := node.Attr("hidefocus")
+	if exists {
+		next, _ := node.Attr("href")
+		next = "http://www.moko.cc" + next
+		//meikongSpider(next)
 	}
 
 }
