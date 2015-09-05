cf57a80cc00f3a4e083b06e1bab96493e71ea216
Rosen Lu <lucumt@gmail.com>
2015-09-05 22:02:19 +0800

    修改爬虫代码，使得在爬取的过程中能够在前端页面显示当前已经爬取的模特图片。

--- goMeikongSpider/src/service/spider.go
+++ goMeikongSpider/src/service/spider.go
@@ -14,7 +14,6 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func EchoServer(url string, num int, dburi string) websocket.Handler {
@@ -28,7 +27,7 @@ func EchoServer(url string, num int, dburi string) websocket.Handler {
 		}
 
 		session.SetMode(mgo.Monotonic, true)
-		modelCollection := session.DB("meikong").C("model")
+		modelCollection := session.DB("meikong").C("modelbackup")
 		//先删除所有的记录
 		modelCollection.RemoveAll(nil)
 		MeikongSpider(ws, url, num, modelCollection)
@@ -53,7 +52,7 @@ func MeikongSpider(ws *websocket.Conn, url string, num int, modelCollection *mgo
 		fmt.Println("点击量:", click)
 		url = "http://www.moko.cc" + href
 		fmt.Println("个人主页:", url)
-		images, imgLogs := getModelInfo(url, name)
+		images := getModelInfo(url, name)
 		clicknum, _ := strconv.Atoi(click)
 
 		modelInfo := "模特" + strconv.Itoa(num) + "." + name + "<br/>职业:" + job + "<br/>点击量:" + click + "<br/>个人主页" + url
@@ -61,8 +60,9 @@ func MeikongSpider(ws *websocket.Conn, url string, num int, modelCollection *mgo
 			log.Println("Can't send")
 		}
 
-		for _, r := range imgLogs {
-			if err = websocket.Message.Send(ws, r); err != nil {
+		for _, r := range images {
+			image := r[strings.LastIndex(r, "/")+1:]
+			if err = websocket.Message.Send(ws, "Create image "+image+" success!<br/><img src=\"模特/"+name+"/"+image+"\" style=\"height:200px;\"/>"); err != nil {
 				log.Println("Can't send")
 				break
 			}
@@ -86,11 +86,10 @@ func MeikongSpider(ws *websocket.Conn, url string, num int, modelCollection *mgo
 	}
 }
 
-func getModelInfo(url string, name string) ([]string, []string) {
+func getModelInfo(url string, name string) []string {
 
 	//利用数组切片来存储图片，避免需要预先定义数组的大小
 	var images []string
-	var imgLogs []string
 
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
@@ -107,22 +106,22 @@ func getModelInfo(url string, name string) ([]string, []string) {
 		if strings.Contains(src, "?") {
 			src = src[:strings.LastIndex(src, "?")]
 		}
-		name := src[strings.LastIndex(src, "/")+1:]
-		imgLogs = append(imgLogs, saveFileFromUrl(src, dir, name))
+		saveFileFromUrl(src, dir, name)
 		images = append(images, src)
 	})
 
-	return images, imgLogs
+	return images
 }
 
-func saveFileFromUrl(url string, dir string, name string) string {
+func saveFileFromUrl(url string, dir string, name string) {
 	response, e := http.Get(url)
 	if e != nil {
 		log.Fatal(e)
 	}
 
 	defer response.Body.Close()
-	file, err := os.Create(dir + string(filepath.Separator) + name)
+	image := url[strings.LastIndex(url, "/")+1:]
+	file, err := os.Create(dir + string(filepath.Separator) + image)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -131,7 +130,5 @@ func saveFileFromUrl(url string, dir string, name string) string {
 		log.Fatal(err)
 	}
 	file.Close()
-	log.Println("create image", name, "success!")
-	logTime := time.Now().Format("2006-01-02 15:04:05")
-	return logTime + " create image " + name + " success!"
+	log.Println("create image", image, "success!")
 }
